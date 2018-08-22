362f67a6fe7e7c5da200616d210e2c1b2521c703
ducvu212 <minhducvu.212@gmail.com>
2018-08-22 15:37:25 +0700

    Init Base MVP

--- app/src/main/java/com/framgia/music_24/screens/main/MainActivity.java
+++ app/src/main/java/com/framgia/music_24/screens/main/MainActivity.java
@@ -1,9 +1,10 @@
-package com.framgia.music_24;
+package com.framgia.music_24.screens.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.framgia.music_24.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainContract.View {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
