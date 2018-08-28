fb43b274ab612fab4bf13b6b5cc4cf06a0061700
DUC_PC <minhducvu.212@gmail.com>
2018-08-28 20:33:38 +0700

    Create UI genre details screen

---
+++ app/src/main/java/com/framgia/music_24/screens/genre/GenreContract.java
@@ -0,0 +1,25 @@
+package com.framgia.music_24.screens.genre;
+
+import com.framgia.music_24.screens.base.BasePresenter;
+
+/**
+ * Created by CuD HniM on 18/08/28.
+ */
+public interface GenreContract {
+
+    /**
+     * View
+     */
+
+    interface View {
+
+    }
+
+    /**
+     * Presenter
+     */
+
+    interface Presenter extends BasePresenter<View> {
+
+    }
+}
---
+++ app/src/main/java/com/framgia/music_24/screens/genre/GenrePresenter.java
@@ -0,0 +1,22 @@
+package com.framgia.music_24.screens.genre;
+
+/**
+ * Created by CuD HniM on 18/08/28.
+ */
+public class GenrePresenter implements GenreContract.Presenter {
+
+    @Override
+    public void setView(GenreContract.View view) {
+
+    }
+
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onStop() {
+
+    }
+}
